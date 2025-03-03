858236581d52519367e69a9d02dabfab779fbc91
Vdavis47 <vdavis47@students.kennesaw.edu>
2025-03-03 02:03:24 -0500

    added the paws to the welcome page

--- src/WelcomeScreen.js
+++ src/WelcomeScreen.js
@@ -5,15 +5,15 @@ import styles from './WelcomeScreen.module.css';
 function WelcomeScreen() {
     const navigate = useNavigate();
   
-    const pawTypes = [
-      { src: "/white paw.png", className: styles.whitepaw },
-      { src: "/light gray paw.png", className: styles.lgpaw },
-      { src: "/gray paw.png", className: styles.graypaw },
-      { src: "/black paw.png", className: styles.blackpaw },
-    ];
+
   
     return(
+      
+    
       <div className={styles.splitScreen}> 
+
+     
+      
          <div className= {styles.welcomescreen}>
         <div className={styles.leftSide}>
         <img src="/companylogo.png" alt="logo" className={styles.logo}/>
@@ -29,23 +29,35 @@ function WelcomeScreen() {
   
         <div className={styles.rightSide}>
           <img src="/welcome dog.png" alt="Welcome Dog" className={styles.AppPuppy}/>
-          
-          {/* Paw prints container */}
-          <div id="paws">
-            {pawTypes.map((paw, i) =>
-              [1, 2, 3, 4].map((num) => (
-                <img
-                  key={`${paw.className}-${num}`}
-                  src={paw.src}
-                  alt={`${paw.className} ${num}`}
-                  className={`${paw.className} paw${num}`}
-                />
-              ))
-            )}
+          <div className={styles.paws}>
+        
+        <img className={styles.whitepaw1} src="/white paw.png" alt="Paw 1" />
+        <img className={styles.whitepaw2} src="/white paw.png" alt="Paw 2" />
+        <img className={styles.whitepaw3} src="/white paw.png" alt="Paw 3" />
+        <img className={styles.whitepaw4} src="/white paw.png" alt=" Paw 4" />
+        
+        <img className={styles.lightgray1} src="/light gray paw.png" alt="Paw 9" />
+        <img className={styles.lightgray2} src="/light gray paw.png" alt="Paw 10" />
+        <img className={styles.lightgray3} src="/light gray paw.png" alt="Paw 11" />
+        <img className={styles.lightgray4} src="/light gray paw.png" alt="Paw 12" />
+        
+         <img className={styles.graypaw1} src="/gray paw.png" alt="Paw 5" />
+        <img className={styles.graypaw2} src="/gray paw.png" alt="Paw 6" />
+        <img className={styles.graypaw3} src="/gray paw.png" alt="Paw 7" />
+        <img className={styles.graypaw4} src="/gray paw.png" alt="Paw 8" />
+
+        <img className={styles.blackpaw1} src="/black paw.png" alt="Black2 Paw 1" />
+        <img className={styles.blackpaw2} src="/black paw.png" alt="Black2 Paw 2" />
+        <img className={styles.blackpaw3} src="/black paw.png" alt="Black2 Paw 3" />
+        <img className={styles.blackpaw4} src="/black paw.png" alt="Black2 Paw 4" />
+
+         </div>
+
+        
           </div>
         </div>
         </div>
-      </div>
+     
     );
   }
 
