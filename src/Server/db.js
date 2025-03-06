98779cc5d42b04cc8de14f845e8a09d4a65592b3
ed-sadler <esadler4@students.kennesaw.edu>
2025-03-06 18:40:51 -0500

    Basic log functionality implemented with Railway DB

--- src/Server/db.js
+++ src/Server/db.js
@@ -4,11 +4,14 @@ require('dotenv').config(); //loads env variables from .env
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    /*
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database:process.env.PG_DATABASE
+    */
 })
 
 
