793253370f1ff75e208851c776aa9f405ee317c3
ed-sadler <esadler4@students.kennesaw.edu>
2025-03-16 15:28:30 -0400

    Added route to allow get /petProfiles request

--- Server/routes/petRoutes.js
+++ Server/routes/petRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { fetchPetProfiles } = require("../controllers/petController")
 
 /* The update route is not safe. Could allow anyone to spam updates.
 For testing purposes only! Dont deploy!!!!
@@ -14,13 +15,23 @@ router.get("/update", async (req, res) => {
         res.json({ message: "Pet data updated successfully!" });
     } catch (error) {
         console.error("Error updating pet data:", error);
-        res.status(500).json({ error: "Failed to update pet data" });
+        res.status(500).json({ error: "Failed to update pet profiles" });
     }
 });
 **************
 */
 
 //Route for frontend to fetch pet data from db
+router.get("/petProfiles", async (req, res) => {
+    try {
+        const petProfiles = await fetchPetProfiles();
+        res.json(petProfiles);
+        
+    } catch (error) {
+        console.error("Error getting pet data:", error);
+        res.status(500).json({ error: "Failed to fetch pet profiles" });
+    }
+});
 
 
 module.exports = router;
