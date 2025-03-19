9c9819427a55b8de865c2e1e84d6bc502b935b74
Victoria Davis <toryd90@gmail.com>
2025-03-19 13:39:07 -0400

    fixed colors for pet bio

--- Client/src/pages/PetBio.js
+++ Client/src/pages/PetBio.js
@@ -45,16 +45,17 @@ return(
 
     <div className={styles.petBio}>  
     
+    
         {/*back button */}
         <div  className={styles.bbutton}>
          <button type="button" onClick={handleBack}><svg 
             xmlns="http://www.w3.org/2000/svg" 
-            width="24" 
+            width="20" 
             height="24" 
             viewBox="0 0 24 24" 
             fill="none" 
             stroke="currentColor" 
-            strokeWidth="2" 
+            strokeWidth="6" 
             strokeLinecap="round" 
             strokeLinejoin="round"
         >
